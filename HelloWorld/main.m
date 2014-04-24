df997446eab20c97943a4dc7bf58a7e2e2f53109
Joanne Fowler <joannefowler@Dan-Fowlers-MacBook-Pro.local>
2014-04-24 18:10:46 -0400

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Joanne Fowler on 4/24/14.
+//  Copyright (c) 2014 Joanne Fowler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
